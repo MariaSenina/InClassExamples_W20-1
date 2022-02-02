c9cad28250129d59a9331fb61e9a970fe44a4571
Maria <maria.senina15@gmail.com>
2022-02-02 14:58:05 -0500

    Add MyListAdapter class + Add row_layout.xml

--- app/src/main/java/com/example/inclassexamples_w20/FirstActivity.java
+++ app/src/main/java/com/example/inclassexamples_w20/FirstActivity.java
@@ -1,8 +1,18 @@
 package com.example.inclassexamples_w20;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -16,6 +26,44 @@ public class FirstActivity extends AppCompatActivity {
 
         ListView myList = findViewById(R.id.theListView);
 
+        MyListAdapter adapter = new MyListAdapter();
+
+        myList.setAdapter(adapter);
+
     }
 
+    class MyListAdapter extends BaseAdapter {
+        private ArrayList<String> elements = new ArrayList<>();
+
+        @Override
+        public int getCount() {
+            return elements.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return "This is row " + position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return (long) position;
+        }
+
+        @Override
+        public View getView(int position, View old, ViewGroup parent) {
+            View newView = old;
+
+            LayoutInflater inflater = getLayoutInflater();
+
+            if (newView == null) {
+                newView = inflater.inflate(R.layout.row_layout, parent, false);
+            }
+
+            TextView tView = newView.findViewById(R.id.textGoesHere);
+            tView.setText(getItem(position).toString());
+
+            return newView;
+        }
+    }
 }
