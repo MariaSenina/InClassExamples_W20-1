be1db90fbc588075b252cfc572837b25011a095e
Maria <maria.senina15@gmail.com>
2022-02-02 16:43:55 -0500

    Set onClickListener to myButton + Set onItemLongClickListener for myList

--- app/src/main/java/com/example/inclassexamples_w20/FirstActivity.java
+++ app/src/main/java/com/example/inclassexamples_w20/FirstActivity.java
@@ -3,11 +3,13 @@ package com.example.inclassexamples_w20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,6 +17,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class FirstActivity extends AppCompatActivity {
+    private ArrayList<String> elements = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +30,32 @@ public class FirstActivity extends AppCompatActivity {
         ListView myList = findViewById(R.id.theListView);
 
         MyListAdapter adapter = new MyListAdapter();
-
         myList.setAdapter(adapter);
 
+        Button myButton = findViewById(R.id.addButton);
+        myButton.setOnClickListener(click -> {
+            elements.add("New element");
+            adapter.notifyDataSetChanged();
+        });
+
+        myList.setOnItemLongClickListener((p, b, pos, id) -> {
+            System.out.println("HIIII");
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("A Title")
+                    .setMessage("Do you want to add stuff?")
+                    .setPositiveButton("Yes", (click, arg) -> {
+                        elements.add("Hello");
+                        adapter.notifyDataSetChanged();
+                    })
+                    .setNegativeButton("No", (click, arg) -> {})
+                    .setNeutralButton("Maybe", (click, arg) -> {})
+                    .setView(getLayoutInflater().inflate(R.layout.row_layout, null))
+                    .create().show();
+            return true;
+        });
     }
 
-    class MyListAdapter extends BaseAdapter {
-        private ArrayList<String> elements = new ArrayList<>();
+    private class MyListAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
